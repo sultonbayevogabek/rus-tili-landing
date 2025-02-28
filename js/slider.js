91c9d9c8b77363294bc7bb80ae8a40e0da869950
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-02-28 21:18:37 +0500

    commit

--- js/slider.js
+++ js/slider.js
@@ -19,9 +19,40 @@ const commentsSlider = new Splide('#comments-slider', {
 
 commentsSlider?.mount();
 
+const certificatesSlider = new Splide('#certificates-slider', {
+  perPage: 2,
+  gap: 20,
+  arrows: false,
+  pagination: false,
+  fixedWidth: '60%',
+  breakpoints: {
+    1300: {
+      fixedWidth: '100%',
+      gap: 16
+    },
+    1200: {
+      fixedWidth: null,
+      perPage: 2,
+      gap: 16
+    },
+    800: {
+      fixedWidth: '60%',
+      gap: 12
+    }
+  },
+  autoplay: true,
+  type: 'loop'
+});
+
+certificatesSlider?.mount();
+
 function navigate(slider, direction) {
   if (slider === 'comments') {
     commentsSlider?.go(direction)
     commentsSlider.Components.Autoplay.pause();
+  }
+  if (slider === 'certificates') {
+    certificatesSlider?.go(direction)
+    certificatesSlider.Components.Autoplay.pause();
   }
 }
