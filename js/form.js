60660c4dc9a0f10a153135f76fdfbbbab15be178
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-03-01 12:41:19 +0500

    commit

--- js/form.js
+++ js/form.js
@@ -104,13 +104,27 @@ forms.forEach(form => {
       submitButton.setAttribute('disabled', true);
       submitButton.textContent = 'Yuborilmoqda...'
 
-      localStorage.setItem('user', JSON.stringify({
+      const user ={
         name,
         phone: '+998' + phone,
         telegram,
         time: new Date().toLocaleString()
-      }))
+      }
 
+      const formData = new FormData();
+
+      formData.append('Ismi', user?.name);
+      formData.append('Telefon raqami', user?.phone);
+      formData.append('Telegrami', user?.telegram);
+      formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
+
+      let response = await fetch('https://script.google.com/macros/s/AKfycbyiXM4n1a5Su9OI-CUoHLND6ELHhiqa35NfellUcsie2q4blMShEsq0w7dnmcKyaJZJpw/exec', {
+        method: 'POST',
+        body: formData
+      })
+      await response.json();
+
+      e.target?.reset()
       submitButton.removeAttribute('disabled');
       submitButton.textContent = "Ro'yxatdan o'tish";
       closeModal();
